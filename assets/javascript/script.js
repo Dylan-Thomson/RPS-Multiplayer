49c199a3e9e7083e77777ff461b8a0cbca40ecb1
Dylan Thomson <djthomson88@gmail.com>
2018-12-10 20:27:00 -0500

    Evaluate moves

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,6 +1,7 @@
 class RockPaperScissors {
-  constructor(database) {
+  constructor(database, moves) {
     this.database = database;
+    this.moves = moves;
   }
 
   run() {
@@ -8,7 +9,9 @@ class RockPaperScissors {
   }
 
   handlePlayerData() {
+    // Handle player data changes
     this.database.ref("players").on("value", (snapshot) => {
+      // Get player data if there is any
       if(snapshot.child("player1").exists()) {
         this.player1 = snapshot.val().player1;
         $("#player1-name").text(this.player1.name);
@@ -23,8 +26,21 @@ class RockPaperScissors {
       else {
         this.player2 = null;
       }
+
+      // Check for moves and compare if both have moved
+      if(snapshot.child("player1").exists() && snapshot.child("player2").exists()) {
+        if(this.player1.move && this.player2.move) {
+          console.log("Both players have moved");
+          console.log("Player1 move: " + this.player1.move);
+          console.log("Player2 move: " + this.player2.move);
+
+          // evaluate moves
+          this.evalMoves();
+        }
+      }
     });
 
+    // Handle player disconnect
     this.database.ref("players").on("child_removed", (snapshot) => {
       console.log(snapshot.val().name, "has disconnected");
       if($("#player1-name").text() === snapshot.val().name) {
@@ -36,6 +52,7 @@ class RockPaperScissors {
     });
   }
 
+  // Decide what to do when a user submits a name
   submitName(name) {
     if(!this.player1) {
       this.addPlayer(name, "player1");
@@ -45,6 +62,7 @@ class RockPaperScissors {
     }
   }
 
+  // Adding a player
   addPlayer(name, player) {
     console.log("Adding", name, "as", player);
     this[player] = {
@@ -54,9 +72,12 @@ class RockPaperScissors {
       losses: 0,
       ties: 0
     }
+
+    // Display player's name and buttons
     $("#" + player + "-name").text(name);
     $("#" + player + "-buttons").removeClass("d-none");
     
+    // Build event listeners for player's buttons
     const db = this.database;
     $("." + player + "-move").on("click", function() {
       // Update player move
@@ -67,13 +88,36 @@ class RockPaperScissors {
       // Hide buttons
       $("#" + player + "-buttons").addClass("d-none");
     });
+
+    // Update database with player data and set to remove on disconnect
     this.database.ref("players/" + player).set(this[player]);
     this.database.ref("players/" + player).onDisconnect().remove();
   }
 
+  evalMoves() {
+    console.log(this.player1.move + " vs " + this.player2.move);
+    const move1 = this.moves.indexOf(this.player1.move);
+    const move2 = this.moves.indexOf(this.player2.move);
+    if(move1 === move2) {
+      console.log("Tie");
+    }
+    else if(move1 === this.moves.length - 1 && move2 === 0) {
+      console.log("Player 2 wins"); // s vs r
+    }
+    else if(move2 === this.moves.length - 1 && move1 === 0) {
+      console.log("Player 1 wins"); // r vs s
+    }
+    else if(move1 > move2) {
+      console.log("Player 1 wins"); // p vs r or s vs p
+    }
+    else {
+      console.log("Player 2 wins"); // r vs p or p vs s
+    }
+  }
+
 }
 
-const game = new RockPaperScissors(database);
+const game = new RockPaperScissors(database, ["r", "p", "s"]);
 $(document).ready(()=> {
   game.run();
 
